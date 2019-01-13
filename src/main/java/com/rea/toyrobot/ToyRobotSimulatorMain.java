82c818933f8e9a7baf58b01a264a16fb4144fd13
Ashish <mcaashish@gmail.com>
2019-01-13 17:26:11 +1100

    build jar as executable jar

--- src/main/java/com/rea/toyrobot/ToyRobotSimulatorMain.java
+++ src/main/java/com/rea/toyrobot/ToyRobotSimulatorMain.java
@@ -9,11 +9,16 @@ public class ToyRobotSimulatorMain
     public static void main( String[] args )
     {
         System.out.println( "Toy Robot Simulator" );
+        System.out.println("Enter your command");
+        System.out.println("Usage: ");
+        System.out.println("PLACE X,Y,NORTH|SOUTH|EAST|WEST, MOVE, LEFT, RIGHT, REPORT");
+        System.out.println("type EXIT/exit to exit");
         Scanner scanner = new Scanner(System.in);
         CommandProcessor processor = new CommandProcessor();
         while(true)
         {
-        	
+        	try
+        	{
         	String input = scanner.nextLine();
         	
         	if(input.equalsIgnoreCase("Exit"))
@@ -25,7 +30,10 @@ public class ToyRobotSimulatorMain
         	{
         		processor.processCommand(input);
         	}
-        	
+        	}catch(IllegalArgumentException e)
+        	{
+        		System.out.println(e.getMessage());
+        	}
         	System.out.println("");
         }
     }
